d276229eda3226accd5a3b953ac88c4c01365a14
naki <192655715+nakicome@users.noreply.github.com>
2025-06-18 23:58:29 +0900

    update: useEffectで全問回答時にページ遷移させるよう対応

--- src/pages/QuizPage.jsx
+++ src/pages/QuizPage.jsx
@@ -1,10 +1,14 @@
+import { useEffect } from "react";
 import Button from "../components/Button/Button";
 import Display from "../components/Display/Display";
 import quizData from "../data/quiz";
+import { RuleTester } from "eslint";
 
 export default function QuizPage() {
     const [quizIndex, setQuizIndex] = useState(0);
     const [answerLogs, setAnswerLogs] = useState([]);
+    const navigation = useNavigate();
+    const MAX_QUIZ_LEN = quizData.length;
 
     const handleClick = (clickedIndex) => {
         if (clickedIndex === quizData[quizIndex].answerIndex) {
@@ -13,7 +17,17 @@ export default function QuizPage() {
             setAnswerLogs((prev) => [...prev, false]);
         }
         setQuizIndex((prev) => prev + 1);
-    }
+    };
+
+    useEffect(() => {
+        if (answerLogs.length === MAX_QUIZ_LEN) {
+            navigation(ROUTES.RESULT, {
+                state: {
+                    maxQuizLen: MAX_QUIZ_LEN,
+                },
+            });
+        }
+    }, [answerLogs]);
 
     return (
         <>
