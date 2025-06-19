58a7f7dd110c405ab130a7d358af27b2c507c9af
naki <192655715+nakicome@users.noreply.github.com>
2025-06-19 22:32:29 +0900

    add: Loadingコンポーネントの追加

--- src/pages/ResultPage.jsx
+++ src/pages/ResultPage.jsx
@@ -1,14 +1,22 @@
+import { useEffect, useState } from "react";
 import { Link, useLocation } from "react-router-dom"
 import { ROUTES } from "../const";
 import Result from "../components/Result/Result";
+import Loading from "../components/Loading/Loading";
 
 export default function ResultPage() {
+    const [active, setActive] = useState(false);
     const location = useLocation();
     const correctNumLen = location.state.correctNumLen;
     const maxQuizLen = location.state.maxQuizLen;
 
+    useEffect(() => {
+        setTimeout(() => setActive(true), 2000);
+    }, []);
+
     return (
         <>
+            <Loading active={active} />
             <h1>Result</h1>
             <Result correctNumLen={correctNumLen} maxQuizLen={maxQuizLen} />
             <br />
